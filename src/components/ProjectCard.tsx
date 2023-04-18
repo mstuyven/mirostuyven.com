f351b0dec7145d8dc2b4de9dcbd76394c9a24f85
mstuyven <mirostuyven@gmail.com>
2023-04-19 00:57:48 +0200

    Add alt and aria-label attributes

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -13,14 +13,14 @@ export function ProjectCard({ title, image, tags, description, link, github }: P
 	return <article class="group relative bg-zinc-800 rounded-md shadow-md overflow-hidden" tabIndex={0}>
 		<div class="flex p-1 items-center">
 			<h3 class="flex-1 px-1">{title}</h3>
-			{link && <a href={link} target="_blank" class="p-1 fill-current hover:fill-zinc-300">{Icons.external_link}</a>}
-			{github && <a href={github} target="_blank" class="p-1 fill-current hover:fill-zinc-300">{Icons.github}</a>}
+			{link && <a href={link} target="_blank" aria-label="External link" class="p-1 fill-current hover:fill-zinc-300">{Icons.external_link}</a>}
+			{github && <a href={github} target="_blank" aria-label="GitHub repository" class="p-1 fill-current hover:fill-zinc-300">{Icons.github}</a>}
 		</div>
 		<div class="flex py-1 px-2 gap-1">
 			{tags?.map(tag => <Tag name={tag} />)}
 		</div>
 		<div class="relative aspect-video bg-zinc-700 cursor-pointer overflow-hidden">
-			<img src={image} class="aspect-video object-cover motion-safe:transition-all group-focus:brightness-[25%] group-focus:scale-150 group-hover:brightness-[25%] group-hover:scale-150" />
+			<img src={image} alt={`Project image for "${title}"`} class="aspect-video object-cover motion-safe:transition-all group-focus:brightness-[25%] group-focus:scale-150 group-hover:brightness-[25%] group-hover:scale-150" />
 			<p class="absolute inset-0 p-2 flex items-center text-center motion-safe:transition-all scale-0 group-focus:scale-100 group-hover:scale-100">{description}</p>
 		</div>
 	</article>
